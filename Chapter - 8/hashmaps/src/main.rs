7763abfb095caab5ce429e12eb4e1dbbc56487cf
Esakki Muthu <esakkimuthu6842@gmail.com>
2024-03-31 15:14:11 +0530

    hashmaps in rust

--- Chapter - 8/hashmaps/src/main.rs
+++ Chapter - 8/hashmaps/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{hash_map, HashMap};
+use std::collections::{ HashMap};
 
 fn main() {
     println!("Hello, world!");
@@ -14,5 +14,12 @@ fn main() {
     Some(i) => println!("{i}"),
     _ =>  println!("Key not found")
    }
+   let mut count_map :HashMap<&str,i32>= HashMap::new();
+   let text = "hello world this is rust";
+   for word in text.split_whitespace(){
+      let count = count_map.entry(word).or_insert(0);
+      *count+=1;
+   }
+   println!("{:?}",count_map);
 
 }

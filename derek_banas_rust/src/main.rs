3bc484d66f2fe9ba6ffd805ae393510362ca8c68
Esakki Muthu <esakkimuthu6842@gmail.com>
2024-05-09 21:11:05 +0530

    next steps in rust

---
+++ derek_banas_rust/src/main.rs
@@ -0,0 +1,13 @@
+#![allow(unused)]
+use std::io::{self, Read};
+use rand::Rng;
+
+fn main(){
+    // println!("Hello, What's your name ? ");
+    // let mut name = String::new();
+    // io::stdin()
+    //     .read_line(&mut name)
+    //     .expect("Oops ! Don't get ur name");
+    // let greeting = " Nice to Meeting you";
+    // println!("Hello {} ! {greeting}",name.trim_end());
+}

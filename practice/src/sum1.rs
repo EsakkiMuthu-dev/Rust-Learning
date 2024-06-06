c360f5e197649ac57251f8d8dd0ed0606419d7c1
Esakki Muthu <esakkimuthu6842@gmail.com>
2024-06-06 12:21:13 +0530

    Update sum1.rs

--- practice/src/sum1.rs
+++ practice/src/sum1.rs
@@ -1,6 +1,7 @@
 pub fn sum_using_iter(){
-    let sum1:i32 = (0..21).into_iter().sum();
+    let sum1:i32 = (0..21).sum();
     let arr = [1,2,3,4,5,6];
     let sum2:i32 = arr.into_iter().sum();
     println!("{} {}",sum1,sum2);
+    println!("{:?} ",arr);
 }

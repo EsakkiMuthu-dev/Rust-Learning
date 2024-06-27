36ea35f57408a1f2f57bf993a72a1cfc12fd342a
Esakki Muthu <esakkimuthu6842@gmail.com>
2024-06-27 07:29:58 +0530

    Second Chapter in learninng rust in th month of lunches

--- learn_rust_in_month_of_lunches/src/main.rs
+++ learn_rust_in_month_of_lunches/src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
-   let float = 12.0;
-   {
-    let float_two = 34.0;
-   }
-   println!("Add to floats {}",float+float_two);
+   // let float = 12.0;
+   // {
+   //  let float_two = 34.0;
+   // }
+   // println!("Add to floats {}",float+float_two);
+   // let my_number = {
+   //    let second_number = 8;
+   //    second_number+9;
+   // };
+   // println!(" My number is {:?}",my_number);
+   // println!(" The smallest of  u128  is {} and the largest of u128  is {}",u128::MIN , u128::MAX);
+   let mut my_number = 12;
+   let num = &mut my_number;
+   *num+=1;
+   let mut word = String::from("Helllo");
+   let mut_word = &mut word;
+   mut_word.push_str("Some other String ".into());
+   println!("My_number is {my_number}");
+   println!(" My string changed to {word}");
 }

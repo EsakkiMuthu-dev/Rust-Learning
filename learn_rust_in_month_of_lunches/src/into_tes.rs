01d0b37af2c8df2efe18aef63bdd1e5ea664e005
Esakki Muthu <esakkimuthu6842@gmail.com>
2024-08-22 07:29:37 +0530

    mem::swap in rust

--- learn_rust_in_month_of_lunches/src/into_tes.rs
+++ learn_rust_in_month_of_lunches/src/into_tes.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 #[derive(Debug)]
 struct Demo{
     name : String
@@ -8,9 +10,18 @@ impl From<String> for Demo {
         Self { name: value }
     }
 }
+impl  Demo{
+    fn change_value(&mut self, new_name:String)
+    {
+        let former = mem::replace(&mut self.name, new_name.clone());
+        println!(" {former} now changed to {new_name}");
+    }
+}
 
 pub fn test_into(){
     let str = String::from("hey!!");
-    let demo: Demo = str.into();
-    println!("{:?}",demo)
+    let mut demo: Demo = str.into();
+    println!("{:?}",demo);
+    demo.change_value("Hello".to_string());
+    println!("{:#?}",demo);
 }

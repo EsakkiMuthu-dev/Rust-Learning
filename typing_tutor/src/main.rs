5771cd691f92eff0c787deb9cb01bf50cf1720e9
Esakki Muthu <esakkimuthu6842@gmail.com>
2024-08-23 10:31:21 +0530

    Update main.rs

--- typing_tutor/src/main.rs
+++ typing_tutor/src/main.rs
@@ -1,6 +1,14 @@
-use crossterm::event::read;
+use crossterm::event::{read, Event, KeyCode, KeyEvent}; 
 fn main() {
-    loop {
-        println!("{:?}", read().unwrap());
+
+    let file_content = std::fs::read_to_string("typing.txt");
+    let typed_content = String::new();
+
+    if let Event::Key(key_event) = read().unwrap(){
+        match key_event.code {
+            KeyCode::Backspace => typed_content.pop(),
+            
+            
+        }
     }
 }
